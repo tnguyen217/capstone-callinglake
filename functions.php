990d414ce8b0654f8797a22841167b60479b7384
tnguyen217 <tnguyen217@nait.ca>
2022-04-04 21:54:20 -0600

    edit css link in function.php & add main.js

--- functions.php
+++ functions.php
@@ -146,10 +146,11 @@ function calling_lake_scripts() {
 
 	// STYLE
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'reset', get_template_directory_uri() . '/css/reset.css') ;
-    wp_enqueue_style( 'styles', get_template_directory_uri() . '/css/styles.css') ;
+    wp_enqueue_style( 'reset', get_template_directory_uri() . '/assets/css/reset.css') ;
+    wp_enqueue_style( 'styles', get_template_directory_uri() . '/assets/css/styles.css') ;
 
 	wp_enqueue_script( 'calling-lake-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
