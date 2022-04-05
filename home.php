462ca7e3d78d0112b05895ed88b4bd29b36e3916
tnguyen217 <tnguyen217@nait.ca>
2022-04-05 02:06:24 -0600

    programs page mobile style completed

--- home.php
+++ home.php
@@ -12,36 +12,30 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) :?>
 	
-			<!-- Banner -->
-			<section class="banner">
-				<?php $banner_image = get_field('banner_image'); ?>
-				<?php if($banner_image): ?>
-					<?php 
-						$img_url = $banner_image['url'];
-					?>
-					<div class="banner-img">
-						<img src="<?php print_r(esc_url($img_url)); ?>">
-					</div>
-				<?php endif; ?>
-
-				<div class="banner-title">
-					<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-					<div class="title-bg"></div>
+		<!-- Banner -->
+		<section class="banner">
+			<?php $banner_image = get_field('banner_image'); ?>
+			<?php if($banner_image): ?>
+				<?php 
+					$img_url = $banner_image['url'];
+				?>
+				<div class="banner-img">
+					<img src="<?php print_r(esc_url($img_url)); ?>">
 				</div>
+			<?php endif; ?>
 
-				<!-- Banner Text -->
-				<?php $banner_text = get_field('banner_text'); ?>
-				<?php if($banner_text): ?>
-					<p><?php _e($banner_text); ?></p>
-				<?php endif; ?>
-			</section>	
+			<div class="banner-title">
+				<h2 class="entry-title">Programs</h2>
+				<div class="title-bg"></div>
+			</div>
 
-		<?php else : ?>
-			<?php get_template_part('template-parts/content', 'none'); ?>
-		<?php endif; ?>
+			<!-- Banner Text -->
+			<?php $banner_text = get_field('banner_text'); ?>
+			<?php if($banner_text): ?>
+				<p><?php _e($banner_text); ?></p>
+			<?php endif; ?>
+		</section>	
 
 		
 		<div class="programs" <?php post_class();?> id="post-<?php the_ID(); ?> ">
