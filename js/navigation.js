59493c672c99c7748a8721f1e92c8629eb4467b6
Max <max4str@gmail.com>
2022-04-04 21:51:20 -0600

    Created header and added some functional css for responsive navigation

--- js/navigation.js
+++ js/navigation.js
@@ -97,3 +97,12 @@
 		}
 	}
 }() );
+
+
+//step 1 - select element the user will click/toggle
+const clickButton = document.querySelector('.toggle-icon');
+
+//step 2 - add the click event
+clickButton.addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('show');
+});
